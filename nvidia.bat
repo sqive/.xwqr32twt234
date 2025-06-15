61e271b95e256c668df42ac0bf4b72fee0bc7e20
sqive <aphinityfynn@gmail.com>
2025-06-15 02:49:38 +0200

    nvidia.bat

--- nvidia.bat
+++ nvidia.bat
@@ -204,4 +204,4 @@ Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "TdrDebug
 Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "TdrLimitCount" /t REG_DWORD /d "0" /f 
 Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "TdrLimitTime" /t REG_DWORD /d "0" /f 
 Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\GraphicsDrivers" /v "TdrTestMode" /t REG_DWORD /d "0" /f 
-timeout /t 1 /nobreak > NUL
+timeout /t 5 /nobreak > NUL
